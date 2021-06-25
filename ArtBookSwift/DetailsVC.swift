ce9467df5ba87bcc5c17aa62d5b3786d66aa5dce
BakytDzhumabev <djumabaevb@gmail.com>
2021-06-26 00:24:10 +0600

    button on right top of navigation bar

---
+++ ArtBookSwift/DetailsVC.swift
@@ -0,0 +1,32 @@
+//
+//  DetailsVC.swift
+//  ArtBookSwift
+//
+//  Created by Bakyt Dzhumabaev on 25/6/21.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    
+    
+    @IBOutlet weak var yearTexxt: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        
+    }
+    
+
+}
