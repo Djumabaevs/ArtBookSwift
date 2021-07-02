1632b1696c3df018b7be88c6cb3a89b5656287c6
BakytDzhumabev <djumabaevb@gmail.com>
2021-07-02 21:19:38 +0600

    UI controller delegate protocol

--- ArtBookSwift/DetailsVC.swift
+++ ArtBookSwift/DetailsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     @IBOutlet weak var yearTexxt: UITextField!
@@ -18,8 +18,25 @@ class DetailsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+        //Recognizers
 
-      
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
+        
+    }
+    
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
     }
     
 
